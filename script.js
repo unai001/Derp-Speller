c02f8704b3e4e21805c9c7a308c75435586a015a
UnaiManzanoPerez <79275029+UnaiManzanoPerez@users.noreply.github.com>
2023-04-12 23:13:32 +0200

    textareas

--- script.js
+++ script.js
@@ -3,20 +3,9 @@ const output = document.getElementById("output");
 
 let inputValue = "";
 
-const initialWidth = 200;
 
 input.addEventListener("input", function (event) {
     inputValue = event.target.value;
-
-    if (this.offsetWidth <= initialWidth) {
-        this.style.width = initialWidth + 'px';
-        output.style.width = initialWidth + 'px';
-    } else if (this.offsetWidth > initialWidth && this.scrollWidth > this.offsetWidth) {
-        this.style.width = (this.scrollWidth + 6) + 'px';
-        output.style.width = (this.scrollWidth + 8) + 'px';
-    }
-
-
     let result = "";
     for (let i = 0; i < inputValue.length; i++) {
         if (i % 2 === 0) {
@@ -31,11 +20,11 @@ input.addEventListener("input", function (event) {
 copyButton.addEventListener('click', function () {
     output.select();
     document.execCommand('copy');
-    this.innerText = 'Copied!';
-    this.style.backgroundColor = 'rgb(36, 179, 0)';
+    this.innerText = 'cOpIeD!';
+    this.style.backgroundColor = 'rgb(0, 172, 29)';
     window.getSelection().removeAllRanges();
     setTimeout(() => {
-        copyButton.innerText = 'Copy to Clipboard';
+        copyButton.innerText = 'cOpY To cLiPbOaRd';
         this.style.backgroundColor = 'rgb(91, 135, 255)';
     }, 2000);
 });
